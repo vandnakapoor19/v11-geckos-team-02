8867e87550695cae19a531a8c6f2dfd5bc7571d4
vandna kapoor <vandnakapoor19@gmail.com>
2019-09-25 16:11:03 -0400

    made changes at home page design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../src/element/header';
 import './App.css';
+import knifeimg from '../src/knife_banner.png'
 
 export default class PersonList extends React.Component {
   constructor(props) {
@@ -52,9 +53,24 @@ export default class PersonList extends React.Component {
           {/* Showing header information here*/}
           <Header />
           {/* End header information here*/}
-          <div className="slider_image"></div>
+          <div className="slider_image">
+            <div id="c506" className="component_ContentDisplay_Content">
+              <div className="plain_content">
+                <h1 ><span >RECIPE INSPIRATION FOR  EVERY OCCASION</span></h1>
+                <h4 >
+                  <span >
+                    <strong>
+                        <img align="baseline" alt="knife banner" height="27" src={knifeimg} title="knife banner" width="711"/>
+                        </strong>
+                      </span>
+                </h4>
+  
+              <h4 ><span >Search hundreds of iron rich beef and lamb recipes</span></h4>
+            </div>
+          </div>
+         </div>
           
-          <div className="clear"></div>
+        <div className="clear"></div>
 
           {/* Showing Content information here*/}
           {(!isrecipe)?'':
